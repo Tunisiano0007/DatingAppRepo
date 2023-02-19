bb73453d91b6d5dabe798966d532024c6e0fd50f
Ramzi Haddad <ramzihaddad007@gmail.com>
2023-02-19 23:41:44 +0100

    Building a walking skeleton Part Two Angular

--- API/Program.cs
+++ API/Program.cs
@@ -5,16 +5,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt=>
+builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-app.UseAuthorization();
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:8500"));
 app.MapControllers();
 
 app.Run();
